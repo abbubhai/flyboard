9b4faf6cd364d9e8b5ead4a18fbded21fdf8fca3
macmohan26 <mrathore@unomaha.edu>
2017-12-02 16:04:31 -0500

    updated static url settings

--- flyboard/settings.py
+++ flyboard/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
